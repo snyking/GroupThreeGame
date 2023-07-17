ee9e4ef7d352e730a7005986ee75971c3902b621
Maya-33 <137533544+Maya-33@users.noreply.github.com>
2023-07-17 18:38:10 +0000

    Deploy React app to GitHub Pages

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -15,20 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// reportWebVitals();
